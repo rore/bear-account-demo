c573680c8f5a79bb2614995a8c6ba11f32f467cc
Rotem Hermon <rotem.hermon@sap.com>
2026-02-18 17:21:33 +0200

    Add impl/test-only change for non-boundary PR scenario

--- src/test/java/com/bear/account/demo/AppTest.java
+++ src/test/java/com/bear/account/demo/AppTest.java
@@ -2,8 +2,15 @@ package com.bear.account.demo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class AppTest {
     @Test
     void placeholder() {
+    }
+
+    @Test
+    void appClassCanBeInstantiated() {
+        assertNotNull(new App());
     }
 }
